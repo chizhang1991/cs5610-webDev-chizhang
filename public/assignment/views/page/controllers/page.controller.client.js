80b93a42680428a4a135bd2c7b03e9f5eb3bfabd
chizhang1991 <gongchangchi@hotmail.com>
2017-06-12 20:33:02 -0700

    new website

--- public/assignment/views/page/controllers/page.controller.client.js
+++ public/assignment/views/page/controllers/page.controller.client.js
@@ -5,9 +5,18 @@
         .controller("NewPageController", NewPageController)
         .controller("EditPageController", EditPageController)
 
-    function PageListController() {
-
+    function PageListController($routeParams, PageService) {
+        var vm = this;
+        // vm.uid = $routeParams.uid;
+        vm.websiteId = $routeParams['wid'];
+        vm.pages = PageService.findPageByWebsiteId(websiteId);
     }
+    // function WebsiteListController($routeParams, WebsiteService) {
+    //     var vm = this;
+    //     vm.uid = $routeParams.uid;
+    //     vm.websites = WebsiteService.findWebsitesByUser(vm.uid);
+    // }
+
     function NewPageController() {
 
     }
--- public/assignment/views/website/controllers/website.controller.client.js
+++ public/assignment/views/website/controllers/website.controller.client.js
@@ -1,20 +1,3 @@
-// (function() {
-//     angular
-//         .module("WebAppMaker")
-//         .controller("WebsiteListController", WebsiteListController)
-//         .controller("NewWebsiteController", NewWebsiteController)
-//         .controller("EditWebsiteController", EditWebsiteController)
-//
-//     function WebsiteListController() {
-//
-//     }
-//     function NewWebsiteController() {
-//
-//     }
-//     function EditWebsiteController() {
-//
-//     }
-// })();
 (function() {
     angular
         .module("WebAppMaker")
@@ -30,10 +13,64 @@
 
     function NewWebsiteController($routeParams, WebsiteService) {
         var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.websites = WebsiteService.findWebsitesByUser(vm.uid);
+
+        vm.newWebsite = newWebsite;
+        function newWebsite(websiteName, websiteDesc) {
+            if (websiteName === undefined || websiteName === null) {
+                vm.error("Website name cannot be empty.");
+                return;
+            }
+            var website = {
+                name: websiteName,
+                desc: websiteDesc
+            }
+            WebsiteService.createWebsite(uid, website);
+        }
     }
+
+    // function ProfileController($routeParams, $location, $timeout, UserService) {
+    //     var vm = this;
+    //     vm.user = UserService.findUserById($routeParams.uid);
+    //     vm.username = vm.user.username;
+    //     vm.firstName = vm.user.firstName;
+    //     vm.lastName = vm.user.lastName;
+    //     vm.email = vm.user.email;
+    //     vm.updateUser = updateUser;
+    //
+    //     function updateUser() {
+    //         var update_user = {
+    //             _id: $routeParams.uid,
+    //             firstName: vm.firstName,
+    //             lastName: vm.lastName,
+    //             email: vm.email
+    //         };
+    //         UserService.updateUser($routeParams.uid, update_user);
+    //         vm.updated = "Profile changes saved!";
+    //
+    //         $timeout(function () {
+    //             vm.updated = null;
+    //         }, 3000);
+    //     }
+    // }
 
     function EditWebsiteController($routeParams, WebsiteService) {
         var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.websites = WebsiteService.findWebsitesByUser(uid);
+
+        vm.updateWebsite = updateWebsite;
+        vm.deleteWebsite = deleteWebsite
+
+        function updateWebsite() {
+            var update_website = {
+                _id: $routeParams.uid,
+            }
+        }
+        function deleteWebsite() {
+
+        }
     }
 
 })();
