1ad5944cfdf418841cc82ee7affa16d06662108a
chizhang1991 <gongchangchi@hotmail.com>
2017-06-14 11:12:32 -0700

    widget list image

--- public/assignment/views/widget/controllers/widget.controller.client.js
+++ public/assignment/views/widget/controllers/widget.controller.client.js
@@ -5,12 +5,24 @@
         .controller("NewWidgetController", NewWidgetController)
         .controller("EditWidgetController", EditWidgetController)
 
-    function WidgetListController() {
+    function WidgetListController($routeParams, $sce, WidgetService) {
+        var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.wid = $routeParams.wid;
+        vm.pid = $routeParams.pid;
+        vm.widgets = WidgetService.findWidgetsByPageId(vm.pid);
 
+        vm.trustThisContent = trustThisContent;
+        function trustThisContent(html) {
+            // diligence to scrub unsafe content
+            $sce.trustAsHtml(html);
+        }
     }
+
     function NewWidgetController() {
 
     }
+
     function EditWidgetController() {
 
     }
