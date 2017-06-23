d709e1446f999706982eb7b92f494251921e645e
chizhang1991 <gongchangchi@hotmail.com>
2017-06-23 11:39:37 -0700

    server service

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -2,3 +2,11 @@
     angular
         .module("WebAppMaker", ["ngRoute"]);
 })();
+
+module.exports = function(app) {
+    require("./services/user.service.server.js")(app);
+    require("./services/website.service.server.js")(app);
+    require("./services/page.service.server.js")(app);
+    require("./services/widget.service.server.js")(app);
+};
+
--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
+// install, load, and configure body parser module
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/public/assignment'));
+
+require("./public/assignment/app")(app);
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
