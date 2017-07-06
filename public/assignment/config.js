0fcfea2e548a5fddada97c800e6c3df48fa38d3b
chizhang1991 <gongchangchi@hotmail.com>
2017-07-06 12:05:02 -0700

    move index back

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,72 +7,72 @@
         $routeProvider
 
             .when('/register', {
-                templateUrl : "assignment/views/user/templates/register.view.client.html",
+                templateUrl : "views/user/templates/register.view.client.html",
                 controller: "RegisterController",
                 controllerAs: "model"
             })
             .when('/login', {
-                templateUrl : "assignment/views/user/templates/login.view.client.html",
+                templateUrl : "views/user/templates/login.view.client.html",
                 controller: "LoginController",
                 controllerAs: "model"
             })
             .when('/user/:uid', {
-                templateUrl : "assignment/views/user/templates/profile.view.client.html",
+                templateUrl : "views/user/templates/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website', {
-                templateUrl : "assignment/views/website/templates/website-list.view.client.html",
+                templateUrl : "views/website/templates/website-list.view.client.html",
                 controller: "WebsiteListController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/new', {
-                templateUrl : "assignment/views/website/templates/website-new.view.client.html",
+                templateUrl : "views/website/templates/website-new.view.client.html",
                 controller: "NewWebsiteController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid', {
-                templateUrl : "assignment/views/website/templates/website-edit.view.client.html",
+                templateUrl : "views/website/templates/website-edit.view.client.html",
                 controller: "EditWebsiteController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page', {
-                templateUrl : "assignment/views/page/templates/page-list.view.client.html",
+                templateUrl : "views/page/templates/page-list.view.client.html",
                 controller: "PageListController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/new', {
-                templateUrl : "assignment/views/page/templates/page-new.view.client.html",
+                templateUrl : "views/page/templates/page-new.view.client.html",
                 controller: "NewPageController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/:pid', {
-                templateUrl : "assignment/views/page/templates/page-edit.view.client.html",
+                templateUrl : "views/page/templates/page-edit.view.client.html",
                 controller: "EditPageController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/:pid/widget', {
-                templateUrl : "assignment/views/widget/templates/widget-list.view.client.html",
+                templateUrl : "views/widget/templates/widget-list.view.client.html",
                 controller: "WidgetListController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/:pid/widget/new', {
-                templateUrl : "assignment/views/widget/templates/widget-chooser.view.client.html",
+                templateUrl : "views/widget/templates/widget-chooser.view.client.html",
                 controller: "NewWidgetController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/:pid/widget/create/:wtype', {
-                templateUrl : "assignment/views/widget/templates/widget-new.view.client.html",
+                templateUrl : "views/widget/templates/widget-new.view.client.html",
                 controller: "CreateWidgetController",
                 controllerAs: "model"
             })
             .when('/user/:uid/website/:wid/page/:pid/widget/:wgid', {
-                templateUrl : "assignment/views/widget/templates/widget-edit.view.client.html",
+                templateUrl : "views/widget/templates/widget-edit.view.client.html",
                 controller: "EditWidgetController",
                 controllerAs: "model"
             })
             .when("/", {
-                templateUrl : "../home.html"
+                templateUrl : "./home.html"
             })
             .otherwise({
                 redirectTo : "/"
