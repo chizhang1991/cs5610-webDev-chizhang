2a19e7186ccf3368b53eeecec548594972005e1e
chizhang1991 <gongchangchi@hotmail.com>
2017-07-21 13:52:19 -0700

    clean model server

--- assignment/app.js
+++ assignment/app.js
@@ -2,8 +2,6 @@
 module.exports = function(app){
 
     var mongoose = require('mongoose');
-    // mongoose.connect('mongodb://localhost/cs5610-webdev');
-    // mongoose.Promise = require('q').Promise;
 
     var models = require("./model/models.server.js")(mongoose);
 
